3b0206f945b0e7ea1f45b36a13e1138e29b09dff
ludihan <65617704+ludihan@users.noreply.github.com>
2025-02-03 16:29:35 -0300

    configuring channels and messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_channel
+
+  def create
+    @channel.messages.create! params.expect(message: [ :content ])
+    redirect_to @channel
+  end
+
+  private
+    def set_post
+      @channel = Channel.find(params[:channel_id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_03_183224) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_03_192012) do
   create_table "channels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_03_183224) do
   end
 
   create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.integer "user_id", null: false
     t.integer "channel_id", null: false
     t.datetime "created_at", null: false
